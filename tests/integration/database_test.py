b5d7cb188466cbc0717b4ace2b73f2804509a9aa
User from AliExpress <118432431+SOLIDusr@users.noreply.github.com>
2023-11-30 21:36:25 +0300

    tests 10/10 tests passed
    OS: Windows 10 64-bit
    Python 3.10
    Pytest 7.4.3

--- tests/integration/database_test.py
+++ tests/integration/database_test.py
@@ -4,14 +4,21 @@ import src.requesto as rq
 
 @pytest.fixture()
 def resource_setup():
+    pass
+
+
+def test_1_pg_table(resource_setup):
     database = rq.postgresqlConnect(host="83.220.170.127",
                                     port="5432",
                                     userName="dolta",
                                     dbName="coredb")
-    return database
+    assert database
+    assert database is not None
 
 
-def test_1_pg_table(resource_setup):
-    assert resource_setup.Table("coredb", resource_setup.cursor)
-    assert resource_setup.Table
+def test_2_sqlt_table(resource_setup):
+    assert rq.sqliteConnection(ifMemory=True)
+    assert rq.sqliteConnection(filename="")
+
+
 
