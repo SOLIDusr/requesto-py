30fd93fc4985041cc1be0b60f940368a8edf566c
NaroMori <reply.naromori@gmail.com>
2023-11-30 13:15:32 +0300

    upd added  vital features
    changelog will be in main branch after test. Be patient!

--- src/requesto.py
+++ src/requesto.py
@@ -1,44 +1,48 @@
 import psycopg2 as pg
 import sqlite3 as sqlt
+import traceback
+import warnings
+import psycopg2.errors as pgerr
 
 
 class DataBase:
     def __init__(self, connection, dbtype: str = None):
         self.connection: DataBase.Connection = DataBase.Connection(connection)
         self.cursor: pg.cursor | sqlt.Cursor = self.connection.getCursor()
-        self.dbtype = dbtype
+        # self.dbtype = dbtype
 
-    def createTable(self, name: str | None = None, columns: dict | None = None):
-        __cursor = self.cursor
-        __row = ""
-
-        def parceType(type: str = None) -> str:
-            if self.dbtype == "sqlite3":
-                match type:
-                    case "str":
-                        return "TEXT"
-                    case "int":
-                        return "INTEGER"
-                    case "float":
-                        return "REAL"
-                    case "date":
-                        return "NUMERIC"
-            else:
-                match type:
-                    case ""
-        for column in columns:
-            __row += "\n     "
-            __row += column
-            __row += " "
-            __row += columns.get(column)
-            __row += ","
-        __row = __row[:-1]
-
-        __cursor.execute(f"""
-        CREATE TABLE IF NOT EXISTS {name} ({__row}
-        );
-        """)
-        return self.Table(name, self)
+    # def createTable(self, name: str | None = None, columns: dict | None = None):
+    #     __cursor = self.cursor
+    #     __row = ""
+    #
+    #     def parceType(type: str = None) -> str:
+    #         if self.dbtype == "sqlite3":
+    #             match type:
+    #                 case "str":
+    #                     return "TEXT"
+    #                 case "int":
+    #                     return "INTEGER"
+    #                 case "float":
+    #                     return "REAL"
+    #                 case "date":
+    #                     return "NUMERIC"
+    #         else:
+    #             match type:
+    #                 case "str":
+    #                     return "VARCHAR(256)"
+    #     for column in columns:
+    #         __row += "\n     "
+    #         __row += column
+    #         __row += " "
+    #         __row += columns.get(column)
+    #         __row += ","
+    #     __row = __row[:-1]
+    #
+    #     __cursor.execute(f"""
+    #     CREATE TABLE IF NOT EXISTS {name} ({__row}
+    #     );
+    #     """)
+    #     return self.Table(name, self)
 
     class Connection:
 
@@ -82,65 +86,131 @@ class DataBase:
         def GetTrStatus(self):
             return self.connection.get_transaction_status()
 
-    class Table:
+        def getTransactionStatus(self):
+            return self.connection.get_transaction_status()
 
-        def __init__(self, name: str, databaseObject):
-            self.__databaseObject: DataBase = databaseObject
+    class Table:
+        def __init__(self, name: str, cursor):
+            self.__cursor: pg.cursor | sqlt.Cursor = cursor
             self.__name = name
 
         def fetchAll(self, param: str = None, params: str = None) -> list:
-            if param is None and params is None:
-                return self.__databaseObject.cursor.execute(f"""SELECT * FROM {self.__name}""").fetchall()
-            elif param is not None and params is None:
-                return self.__databaseObject.cursor.execute(f"""SELECT {param} FROM {self.__name}""").fetchall()
-            elif param is None and params is not None:
-                return (self.__databaseObject.cursor.execute(f"""SELECT * FROM {self.__name} WHERE """ + params)
-                        .fetchall())
-            elif param is not None and params is not None:
-                return (self.__databaseObject.cursor.execute(f"""SELECT {param} FROM {self.__name} WHERE""" + params)
-                        .fetchall())
-            else:
-                raise DataBase.WrongParamError
+            try:
+                if param is None and params is None:
+                    self.__cursor.execute(f"""SELECT * FROM {self.__name}""")
+                elif param is not None and params is None:
+                    self.__cursor.execute(f"""SELECT {param} FROM {self.__name}""")
+                elif param is None and params is not None:
+                    self.__cursor.execute(f"""SELECT * FROM {self.__name} WHERE """ + params)
+                elif param is not None and params is not None:
+                    self.__cursor.execute(f"""SELECT {param} FROM {self.__name} WHERE """ + params)
+                else:
+                    raise DataBase.WrongParamError
+                return self.__cursor.fetchall()
+            except AttributeError as AE:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return []
+
+            except DataBase.WrongParamError as WPE:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return []
+            
+            except not DataBase.WrongParamError and AttributeError:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return []
 
         def fetchMany(self, param: str | None = None, params: str | None = None, size: int | None = 0) -> list:
-            if param is None and params is None:
-                return self.__databaseObject.cursor.execute(f"""SELECT * FROM {self.__name}""").fetchmany(size)
-            elif param is not None and params is None:
-                return self.__databaseObject.cursor.execute(f"""SELECT {param} FROM {self.__name}""").fetchmany(size)
-            elif param is None and params is not None:
-                return (self.__databaseObject.cursor.execute(f"""SELECT * FROM {self.__name} WHERE """ + params)
-                        .fetchmany(size))
-            elif param is not None and params is not None:
-                return (self.__databaseObject.cursor.execute(f"""SELECT {param} FROM {self.__name} WHERE""" + params)
-                        .fetchmany(size))
-            else:
-                raise DataBase.WrongParamError
+            try:
+                assert size is not None and size > 0
+                if param is None and params is None:
+                    self.__cursor.execute(f"""SELECT * FROM {self.__name}""")
+                elif param is not None and params is None:
+                    self.__cursor.execute(f"""SELECT {param} FROM {self.__name}""")
+                elif param is None and params is not None:
+                    self.__cursor.execute(f"""SELECT * FROM {self.__name} WHERE """ + params)
+                elif param is not None and params is not None:
+                    self.__cursor.execute(f"""SELECT {param} FROM {self.__name} WHERE """ + params)
+                else:
+                    raise DataBase.WrongParamError
+                return self.__cursor.fetchmany(size)
+            except AttributeError as AE:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return []
+            except DataBase.WrongParamError as WPE:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return []
+            except AssertionError:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return []
 
         def fetchOne(self, param: str | None = None, params: str | None = None) -> tuple:
-            if param is None and params is None:
-                return self.__databaseObject.cursor.execute(f"""SELECT * FROM {self.__name}""").fetchone()
-            elif param is not None and params is None:
-                return self.__databaseObject.cursor.execute(f"""SELECT {param} FROM {self.__name}""").fetchone()
-            elif param is None and params is not None:
-                return (self.__databaseObject.cursor.execute(f"""SELECT * FROM {self.__name} WHERE """ + params)
-                        .fetchone())
-            elif param is not None and params is not None:
-                return (self.__databaseObject.cursor.execute(f"""SELECT {param} FROM {self.__name} WHERE""" + params)
-                        .fetchone())
-            else:
-                raise DataBase.WrongParamError
+            try:
+                if param is None and params is None:
+                    self.__cursor.execute(f"""SELECT * FROM {self.__name}""")
+                elif param is not None and params is None:
+                    self.__cursor.execute(f"""SELECT {param} FROM {self.__name}""")
+                elif param is None and params is not None:
+                    self.__cursor.execute(f"""SELECT * FROM {self.__name} WHERE """ + params)
+                elif param is not None and params is not None:
+                    self.__cursor.execute(f"""SELECT {param} FROM {self.__name} WHERE """ + params)
+                else:
+                    raise DataBase.WrongParamError
+                return self.__cursor.fetchone()
+            except AttributeError as AE:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return ()
+            except DataBase.WrongParamError as WPE:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return ()
 
-        def insert(self, params: str | None = None, values: str | None = None):
-            assert params is not None
-            assert values is not None
-            return self.__databaseObject.cursor.execute(f"""INSERT INTO {self.__name} ({params}) VALUES {values}""")
+        def insert(self, params: str | None = None, values: str | None = None) -> bool:
+            try:
+                assert params is not None
+                assert values is not None
+                request = f"""INSERT INTO {self.__name} ({params}) VALUES {values}"""
+                self.__cursor.execute(request)
+                return True
+            except pgerr.UniqueViolation as UV:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return False
+            except not pgerr.UniqueViolation as NUV:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return False
+            
+        def update(self, params: str | None = None, values: str | None = None, where: str | None = None) -> bool:
+            try:
+                assert params is not None and values is not None
+                if where is None:
+                    request = f"""UPDATE {self.__name} SET ({params}) = ({values})"""
+                else:
+                    request = f"""UPDATE {self.__name} SET ({params}) = ({values}) WHERE {where}"""
+                self.__cursor.execute(request)
+            except AssertionError:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return False
+            except pgerr.Error:
+                trace = traceback.format_exc()
+                warnings.warn(trace)
+                return False
 
     class WrongParamError(Exception):
         pass
 
 
 def postgresqlConnect(host, port, dbName, userName) -> DataBase:
-    userPass: str = input("Enter database password\n>")
+    userPass: str = input(f"Input Database password\n"
+                          f"{userName}@{host}({dbName})$")
     connection = pg.connect(
         host=host,
         user=userName,
