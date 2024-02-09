b5f71f1f49f7505c17cf2f41d56beb1765297526
NaroMori <reply.naromori@gmail.com>
2024-02-09 15:34:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import src.requesto as rq
 
 
+db = rq.postgresqlConnect(None, "83.220.170.127", "5432", "dolta", "coredb")
 table = rq.DataBase.Table("userdata", db.cursor)
 times = []
 default = time.time_ns() // 1_000_000
